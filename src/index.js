7ca8c73e706b831cdcaf6f8b45bffaabf9f9fa23
Lucas Bispo <113217544+unoprogramado@users.noreply.github.com>
2023-12-03 00:33:54 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ app.use(express.static(__dirname + '../../' + '/public'));
 
 app.engine("handlebars", exphbs.engine({
     defaultLayout: "main",
-    layoutsDir: path.join(__dirname, "views", "layouts")
+    // layoutsDir: path.join(__dirname, "views", "layouts")
 }));
 app.set("view engine", "handlebars");
-app.set("views", path.join(__dirname, "views"));
+// app.set("views", path.join(__dirname, "views"));
 
 app.use(router);
 // app.get("/login",function(req, res){
